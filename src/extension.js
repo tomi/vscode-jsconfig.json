4667ed724c630d5c22acc419b0f8efe11de6e467
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-03-01 18:35:00 +0200

    Execute Reload Javascript command automatically when jsconfig.json is created

--- src/extension.js
+++ src/extension.js
@@ -3,6 +3,7 @@
 const vscode    = require("vscode");
 const window    = vscode.window;
 const workspace = vscode.workspace;
+const commands  = vscode.commands;
 
 const Q    = require("q");
 const fs   = require("./promise-fs");
@@ -32,25 +33,33 @@ function fixCommand() {
         window.showErrorMessage(`Cant create ${FILE_NAME}. No folder opened.`);
         return;
     }
-    
+
+    let wasCreated = false;
+
     const jsconfigPath = getJsconfigPath();
     fs.fileExists(jsconfigPath)
         .then(exists => {
             if (exists) {
                 return Q(undefined);
             } else {
+                wasCreated = true;
                 return fs.writeFile(jsconfigPath, FILE_CONTENTS);
             }
         })
         .then(() => workspace.openTextDocument(jsconfigPath))
         .then((doc) => window.showTextDocument(doc))
+        .then(() => {
+            if (wasCreated) {
+                commands.executeCommand("javascript.reloadProjects");
+            }
+        })
         .fail((error) => window.showInformationMessage(`Error while creating ${FILE_NAME}: ${error}`));
 }
 
 function activate(context) {
 	const disposable = vscode.commands.registerCommand(
         "extension.createJsconfig", fixCommand);
-    
+
 	context.subscriptions.push(disposable);
 }
 
